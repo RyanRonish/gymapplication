87c710cdfd1e7baa534b59c7482463c9b3251d47
RyanRonish <roni0210@bears.unco.edu>
2024-09-26 10:03:30 -0600

    setting change again

--- project/config/settings.py
+++ project/config/settings.py
@@ -116,9 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
- # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "gym/static",  # This ensures Django knows where to look for your static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
