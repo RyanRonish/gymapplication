0df21db4a59eadbfe7a172d0006f6752b7eeb06c
RyanRonish <roni0210@bears.unco.edu>
2024-09-26 09:45:44 -0600

    settings change

--- project/config/settings.py
+++ project/config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
  # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
