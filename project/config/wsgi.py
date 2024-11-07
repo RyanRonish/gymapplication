f28ae1e6d2e48878b5db885bb670264618852bac
RyanRonish <roni0210@bears.unco.edu>
2024-11-07 02:23:55 -0700

    wsgi edits

--- project/config/wsgi.py
+++ project/config/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
