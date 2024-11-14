b00c00369b353465d564f02df39810368e0ce0ad
RyanRonish <roni0210@bears.unco.edu>
2024-11-14 16:46:59 -0700

    settings change

--- project/02/config/settings.py
+++ project/02/config/settings.py
@@ -145,12 +145,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "/static",  # This ensures Django knows where to look for your static files
-]
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+#    BASE_DIR / "/static",  # This ensures Django knows where to look for your static files
+#]
 
 import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Adjust this to match the actual path of your static files
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
