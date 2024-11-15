856ef426317147588c9d196249e2834304190a79
RyanRonish <roni0210@bears.unco.edu>
2024-11-14 17:05:52 -0700

    media root change test

--- project/02/config/settings.py
+++ project/02/config/settings.py
@@ -172,4 +172,5 @@ LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = BASE_DIR / 'media'
